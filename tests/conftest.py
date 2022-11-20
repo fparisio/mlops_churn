8c43ccd92699aedce16b07ef2579f12da7215270
Francesco Parisio <francesco.parisio@protonmail.com>
2022-11-20 09:46:33 +0100

    Remove tests from linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,39 +6,16 @@ import pytest
 
 
 def pytest_addoption(parser):
-    """Parser adoption.
-
-    Parameters
-    ----------
-    parser : Any
-        configuration for parser adoption
-    """
     parser.addoption(
         "--runslow", action="store_true", default=False, help="run slow tests"
     )
 
 
 def pytest_configure(config):
-    """Configure pytest.
-
-    Parameters
-    ----------
-    config : any
-        configuration for pytest
-    """
     config.addinivalue_line("markers", "slow: mark test as slow to run")
 
 
 def pytest_collection_modifyitems(config, items):
-    """Pytest collection.
-
-    Parameters
-    ----------
-    config : Any
-        configuration
-    items : Any
-        items
-    """
     if config.getoption("--runslow"):
         # --runslow given in cli: do not skip slow tests
         return
